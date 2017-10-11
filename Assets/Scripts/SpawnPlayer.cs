00e6e4b3ee62aafca7ec671ae13ff05454b3eebe
Gunther <amgunther1s@semo.edu>
2017-10-11 16:45:30 -0500

    Added player movement and animation
    The player can now move in any direction and the player sprite will
    change in order to reflect the direction we are moving in

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -28,18 +28,28 @@ public class SpawnPlayer : MonoBehaviour {
 		if (Input.GetKeyDown ("d")) {
 			s = obj.GetComponent<SpriteRenderer>();
 			s.flipX = false;
-			obj.transform.position = new Vector3 (obj.position.x + 1.0f, obj.position.y, obj.position.z); 
+			obj.transform.position = new Vector3 (obj.position.x + 6.2f, obj.position.y, obj.position.z);
+			obj.transform.rotation = Quaternion.Euler(obj.transform.rotation.x, obj.transform.rotation.y, 0.0f);
 		}
 		if (Input.GetKeyDown ("w")) {
-			obj.transform.position = new Vector3 (obj.position.x, obj.position.y+1.0f, obj.position.z); 
+			obj.transform.position = new Vector3 (obj.position.x, obj.position.y+8.0f, obj.position.z); 
+			if(s.flipX == false)
+				obj.transform.rotation = Quaternion.Euler(obj.rotation.x, obj.rotation.y, 90.0f);
+			else
+				obj.transform.rotation = Quaternion.Euler(obj.rotation.x, obj.rotation.y, -90.0f);
 		}
 		if (Input.GetKeyDown ("a")) {
 			s = obj.GetComponent<SpriteRenderer>();
 			s.flipX = true;
-			obj.transform.position = new Vector3 (obj.position.x - 1.0f, obj.position.y, obj.position.z); 
+			obj.transform.position = new Vector3 (obj.position.x - 6.2f, obj.position.y, obj.position.z);
+			obj.transform.rotation = Quaternion.Euler(obj.rotation.x, obj.rotation.y, 0.0f);
 		}
 		if (Input.GetKeyDown ("s")) {
-			obj.transform.position = new Vector3 (obj.position.x, obj.position.y - 1.0f, obj.position.z); 
+			obj.transform.position = new Vector3 (obj.position.x, obj.position.y - 8.0f, obj.position.z);
+			if(s.flipX == false)
+				obj.transform.rotation = Quaternion.Euler(obj.rotation.x, obj.rotation.y, -90.0f);
+			else
+				obj.transform.rotation = Quaternion.Euler(obj.rotation.x, obj.rotation.y, 90.0f);
 		}
 	}
 }
