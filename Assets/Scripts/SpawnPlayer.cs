e0696e60b5c69daf0aab509721df1743492b28d7
Gunther <amgunther1s@semo.edu>
2017-10-11 09:54:10 -0500

    Added camera controller to player spawn script
    Allows the main camera to follow the player after the player is spawned
    into the game

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class SpawnPlayer : MonoBehaviour {
 	public GameObject Spawns;
 	private Transform obj;
+	private Vector3 offset;   
 	void Start () {
 		int random = Random.Range(0, Spawns.transform.childCount);
 		print (random);
 		obj = Spawns.transform.GetChild (random);
 		obj.name = "Player";
 		Spawns.transform.GetChild (random).gameObject.SetActive (true);
+		offset = transform.position - obj.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		transform.position = obj.transform.position + offset;
 	}
 }
