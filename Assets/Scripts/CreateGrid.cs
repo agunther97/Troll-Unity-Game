d44189a4362d75f3887d345ff1022eba3497572c
rocke97 <aarongunther97@gmail.com>
2018-02-09 15:54:16 -0600

    Started complete rework of the project

--- Assets/Scripts/CreateGrid.cs
+++ Assets/Scripts/CreateGrid.cs
@@ -2,397 +2,137 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateGrid : MonoBehaviour {
-	string line1 = "#########################################################################";
-	string line2 = "#   #               #               #           #                   #   #";
-	string line3 = "#   #   #########   #   #####   #########   #####   #####   #####   #   #";
-	string line4 = "#               #       #   #           #           #   #   #       #   #";
-	string line5 = "#########   #   #########   #########   #####   #   #   #   #########   #";
-	string line6 = "#       #   #               #           #   #   #   #   #           #   #";
-	string line7 = "#   #   #############   #   #   #########   #####   #   #########   #   #";
-	string line8 = "#   #               #   #   #       #           #           #       #   #";
-	string line9 = "#   #############   #####   #####   #   #####   #########   #   #####   #";
-	string line10 ="#           #       #   #       #   #       #           #   #           #";
-	string line11 ="#   #####   #####   #   #####   #   #########   #   #   #   #############";
-	string line12 ="#       #       #   #   #       #       #       #   #   #       #       #";
-	string line13 ="#############   #   #   #   #########   #   #####   #   #####   #####   #";
-	string line14 ="#           #   #           #       #   #       #   #       #           #";
-	string line15 ="#   #####   #   #########   #####   #   #####   #####   #############   #";
-	string line16 ="#   #       #           #           #       #   #   #               #   #";
-	string line17 ="#   #   #########   #   #####   #########   #   #   #############   #   #";
-	string line18 ="#   #           #   #   #   #   #           #               #   #       #";
-	string line19 ="#   #########   #   #   #   #####   #########   #########   #   #########";
-	string line20 ="#   #       #   #   #           #           #   #       #               #";
-	string line21 ="#   #   #####   #####   #####   #########   #####   #   #########   #   #";
-	string line22 ="#   #                   #           #               #               #   #";
-	string line23 ="# X #####################################################################";
-	public GameObject firstwall;
-	public GameObject wallparent;
-	private GameObject obj;
-	private float temp = 0;
-	private float temp2 = 16f;
-	int j = 83;
-	int count = 0;
-	// Use this for initialization
-	void Start () {
-		for (int i = 0; i < line3.Length; i++) {
-			if (line3 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
+public class CreateGrid : MonoBehaviour 
+{
+	List<List<Tile>> map = new List<List<Tile>>();
+	Stack<Tile> visitedTiles = new Stack<Tile>();
+	[Tooltip("The number of rows or cols in our maze, must be odd numbers, will scale up if an even # is entered")]
+	public int rows;
+	public int cols;
+	//the rows and cols of our grid
+
+	struct Point {
+		public int x { get; set;}
+		public int y { get; set; }
+		public Point(int p_x, int p_y)
+		{
+			x = p_x;
+			y = p_y;
 		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line4.Length; i++) {
-			if (line4 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
+	}
+
+	public void MazeDriver()
+	{
+		EnsureOddSize(); // ensure that the grid has an odd number for rows and cols
+		PopulateMapWithWalls();
+		GenerateMaze();
+	}
+
+	private void EnsureOddSize()
+	{
+		if (rows % 2 == 0)
+			rows++;
+		if (cols % 2 == 0)
+			cols++;
+	}
+
+	private void PopulateMapWithWalls()
+	{
+		foreach (Tile tile in map) {
+			tile.isWall = true;
+			if (tile.x == 0 || tile.x == 1 )
+				tile.moveWest = false;
+			if (tile.x == rows - 1 || tile.x == rows - 2)
+				tile.moveEast = false;
+			if (tile.y == 0 || tile.y == 1)
+				tile.moveSouth = false;
+			if (tile.y == cols - 1 || tile.y == cols - 2)
+				tile.moveNorth = false;
 		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line5.Length; i++) {
-			if (line5 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line6.Length; i++) {
-			if (line6 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line7.Length; i++) {
-			if (line7 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line8.Length; i++) {
-			if (line8 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line9.Length; i++) {
-			if (line9 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line10.Length; i++) {
-			if (line10 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line11.Length; i++) {
-			if (line11 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line12.Length; i++) {
-			if (line12 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line13.Length; i++) {
-			if (line13 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line14.Length; i++) {
-			if (line14 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line15.Length; i++) {
-			if (line15 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line16.Length; i++) {
-			if (line16 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line17.Length; i++) {
-			if (line17 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line18.Length; i++) {
-			if (line18 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line19.Length; i++) {
-			if (line19 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line20.Length; i++) {
-			if (line20 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line21.Length; i++) {
-			if (line21 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line22.Length; i++) {
-			if (line22 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
-			}
-			temp = temp + 6.2f;
-		}
-		temp2 = temp2 + 8f;
-		temp = 0;
-		for (int i = 0; i < line23.Length; i++) {
-			if (line23 [i] == '#') {
-				count++;
-				j = j + count;
-				obj = Instantiate (firstwall, wallparent.transform);
-				Vector3 newPos = new Vector3 (temp, temp2, firstwall.transform.position.z);
-				obj.transform.position = newPos;
-				obj.name = "Wall (" + j + ")";
-				count = 0;
-				firstwall = obj;
-				temp = temp + 6.2f;
-				continue;
+	}
+
+	private void GenerateMaze()
+	{
+		Tile startTile = GrabRandomTile();
+		visitedTiles.Push(startTile); // put our first tile on the stack
+		bool running = true;
+		while (running) {
+			startTile = GetRandomPath(startTile);
+			if (startTile == null) {
+				return;
 			}
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private Tile GrabRandomTile()
+	{
+		int randomX = Random.Range(0, rows-1);
+		int randomY = Random.Range(0, cols-1);
+		return map[randomX][randomY];
+	}
+
+	private Tile GetRandomPath(Tile startTile)
+	{
+		List<int> validDirections = GetValidDirections(startTile);
+		if (validDirections.Count == 0) {
+			if (visitedTiles.Count == 0)
+				return null;
+			visitedTiles.Pop();
+			return visitedTiles.Peek();
+		}
+		int randomDirectionIndex = Random.Range(0, validDirections - 1);
+		int direction = validDirections[randomDirectionIndex];
+		switch (direction) {
+			case 0:
+				startTile.moveNorth = false;
+				Tile northTile = map.GetNorthTile(startTile);
+				northTile.isWall = false;
+				northTile.moveNorth = false;
+				map.GetNorthTile(northTile).isWall = false;
+				visitedTiles.Push(map.GetNorthTile(northTile));
+				return map.GetNorthTile(northTile);
+				break;
+			case 1:
+				startTile.moveSouth = false;
+				Tile southTile = map.GetSouthTile(startTile);
+				southTile.isWall = false;
+				southTile.moveSouth = false;
+				map.GetSouthTile(southTile).isWall = false;
+				visitedTiles.Push(map.GetSouthTile(southTile));
+				return map.GetSouthTile(southTile);
+				break;
+			case 2:
+				startTile.moveEast = false;
+				Tile eastTile = map.GetEastTile(startTile);
+				eastTile.isWall = false;
+				eastTile.moveEast = false;
+				map.GetEastTile(eastTile).isWall = false;
+				visitedTiles.Push(map.GetEastTile(eastTile));
+				return map.GetEastTile(eastTile);
+				break;
+			case 3:
+				startTile.moveWest = false;
+				Tile westTile = map.GetWestTile(startTile);
+				westTile.isWall = false;
+				westTile.moveWest = false;
+				map.GetWestTile(westTile).isWall = false;
+				visitedTiles.Push(map.GetWestTile(westTile));
+				return map.GetWestTile(westTile);
+				break;
+		}
+	}
+
+	private List<int> GetValidDirections(Tile startTile)
+	{
+		List<int> directions = new List<int>();
+		if (startTile.moveNorth)
+			directions.Add(0);
+		if (startTile.moveSouth)
+			directions.Add(1);
+		if (startTile.moveEast)
+			directions.Add(2);
+		if (startTile.moveWest)
+			directions.Add(3);
+		return directions;
 	}
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+	public int x { get; set; }
+	public int y { get; set; }
+	public bool isOccupied { get; set; }
+	public bool isWall{ get; set; }
+	public bool moveNorth { get; set; } = true;
+	public bool moveSouth { get; set; } = true;
+	public bool moveEast { get; set; } = true;
+	public bool moveWest { get; set; } = true;
+}
